d9935ca204926d951cbeecb3653a1290e4e67f3c
EC2 Default User <ec2-user@ip-172-31-23-245.ec2.internal>
2020-09-17 00:45:47 +0000

    Part 1 & 2

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -19,7 +19,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  assert page.body =~ /#{e1}.*#{e2}/m
+  #fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -30,10 +31,14 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split.each do |a|
+    uncheck == true ? uncheck(a) : check(r)
+  end
+  #fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  page.all('tbody tr').count.should == Movie.count
+  #fail "Unimplemented"
 end
