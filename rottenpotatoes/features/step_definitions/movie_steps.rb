7ede4b59acd12b6d6ff9e4f235f97448b31bdc10
EC2 Default User <ec2-user@ip-172-31-23-245.ec2.internal>
2020-09-17 01:25:44 +0000

    part3

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -19,6 +19,10 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
+  #body = page.body
+  #e1_index = body.index(e1)
+  #e2_index = body.index(e2)
+  #assert e1_index < e2_index
   assert page.body =~ /#{e1}.*#{e2}/m
   #fail "Unimplemented"
 end
@@ -31,8 +35,13 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  rating_list.split.each do |a|
-    uncheck == true ? uncheck(a) : check(r)
+  rating_list.split(" ").each do |a|
+    a = "ratings_"+a
+    if uncheck
+      uncheck(a)
+    else
+      check(a)
+    end
   end
   #fail "Unimplemented"
 end
